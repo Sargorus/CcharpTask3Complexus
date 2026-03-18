2c5b3a50ba8c60f7040bf0b9fd8b34069615ca20
Sargorus <Dima35206@yandex.ru>
2026-03-18 12:14:00 +0800

    Добавлен класс для комплексного числа (только основа) и один тест на проверку его вывода в string

--- CcharpTask3Complexus/Class1.cs
+++ CcharpTask3Complexus/Class1.cs
@@ -1,8 +1,31 @@
 
 namespace CcharpTask3Complexus
 {
-    public class Class1
+    /*
+    Комплексное число
+
+    сложение
+     вычитание
+      умножение
+       деление двух комплексных чисел
+         сравнение (как сравнение векторов соотвествующих комплексному числу)
+    */
+    public class ComplexNumberDouble
     {
+        // a + b * i
+        private double realValueA;
+        private double realValueB;
+
+        public ComplexNumberDouble(double realValueA, double realValueB)
+        {
+            this.realValueA = realValueA;
+            this.realValueB = realValueB;
+        }
+
+        public string ComplexNumberDoubleToString()
+        {
+            return String.Format("Вещественная часть {0}, Мнимая {1}", this.realValueA, realValueB);
+        }
     }
 
 }
---
+++ CcharpTask3ComplexusTests/ComplexNumberDoubleTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CcharpTask3Complexus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CcharpTask3Complexus.Tests
+{
+    [TestClass()]
+    public class ComplexNumberDoubleTests
+    {
+
+        [TestMethod()]
+        public void ComplexNumberDoubleToStringTest()
+        {
+            var testComplexNumber = new ComplexNumberDouble(12, 122);
+            Assert.AreEqual("Вещественная часть 12, Мнимая 122", testComplexNumber.ComplexNumberDoubleToString());
+
+        }
+    }
+}
