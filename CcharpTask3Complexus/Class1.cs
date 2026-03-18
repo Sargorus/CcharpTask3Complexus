e51d03dcca2036bfc7452c6f570100d8d2b09bd3
Sargorus <Dima35206@yandex.ru>
2026-03-18 12:41:43 +0800

    Добавлены операции сложения комплексного числа с комплексным числом, с int, с float, с double. Добавлен операции вычитания комплексного числа от комплексного числа

--- CcharpTask3Complexus/Class1.cs
+++ CcharpTask3Complexus/Class1.cs
@@ -9,6 +9,10 @@ namespace CcharpTask3Complexus
       умножение
        деление двух комплексных чисел
          сравнение (как сравнение векторов соотвествующих комплексному числу)
+
+    справка:
+    (два комплексных числа равны между собой тогда и только тогда, когда равны их вещественные и мнимые части).
+
     */
     public class ComplexNumberDouble
     {
@@ -25,6 +29,68 @@ namespace CcharpTask3Complexus
         public string ComplexNumberDoubleToString()
         {
             return String.Format("Вещественная часть {0}, Мнимая {1}", this.realValueA, realValueB);
+        }
+
+        public static ComplexNumberDouble operator +(ComplexNumberDouble a, ComplexNumberDouble b)
+        {
+            var newValueA = a.realValueA + b.realValueA;
+            var newValueB = a.realValueB + b.realValueB;
+
+            var ComplexNumberDoubleReturn = new ComplexNumberDouble(newValueA, newValueB);
+
+            return ComplexNumberDoubleReturn;
+        }
+
+
+        public static ComplexNumberDouble operator +(ComplexNumberDouble a, int b)
+        {
+
+            var ComplexNumberDoubleReturn = new ComplexNumberDouble(a.realValueA + b, a.realValueB);
+            return ComplexNumberDoubleReturn;
+        }
+
+        public static ComplexNumberDouble operator +(int a, ComplexNumberDouble b)
+        {
+
+            var ComplexNumberDoubleReturn = new ComplexNumberDouble(b.realValueA + a, b.realValueB);
+            return ComplexNumberDoubleReturn;
+        }
+
+
+        public static ComplexNumberDouble operator +(ComplexNumberDouble a, float b)
+        {
+
+            var ComplexNumberDoubleReturn = new ComplexNumberDouble(a.realValueA + b, a.realValueB);
+            return ComplexNumberDoubleReturn;
+        }
+        public static ComplexNumberDouble operator +(float a, ComplexNumberDouble b)
+        {
+
+            var ComplexNumberDoubleReturn = new ComplexNumberDouble(b.realValueA + a, b.realValueB);
+            return ComplexNumberDoubleReturn;
+        }
+
+
+        public static ComplexNumberDouble operator +(ComplexNumberDouble a, double b)
+        {
+
+            var ComplexNumberDoubleReturn = new ComplexNumberDouble(a.realValueA + b, a.realValueB);
+            return ComplexNumberDoubleReturn;
+        }
+        public static ComplexNumberDouble operator +(double a, ComplexNumberDouble b)
+        {
+
+            var ComplexNumberDoubleReturn = new ComplexNumberDouble(b.realValueA + a, b.realValueB);
+            return ComplexNumberDoubleReturn;
+        }
+
+
+        public static ComplexNumberDouble operator -(ComplexNumberDouble a, ComplexNumberDouble b)
+        {
+            b.realValueA *= -1;
+            b.realValueB *= -1;
+
+            return a + b;
         }
     }
 
--- CcharpTask3ComplexusTests/ComplexNumberDoubleTests.cs
+++ CcharpTask3ComplexusTests/ComplexNumberDoubleTests.cs
@@ -17,7 +17,43 @@ namespace CcharpTask3Complexus.Tests
         {
             var testComplexNumber = new ComplexNumberDouble(12, 122);
             Assert.AreEqual("Вещественная часть 12, Мнимая 122", testComplexNumber.ComplexNumberDoubleToString());
-
         }
+
+        [TestMethod()]
+        public void ComplexNumberDoubleAddingComplex()
+        {
+            var testComplexNumberA = new ComplexNumberDouble(12, 122);
+            var testComplexNumberB = new ComplexNumberDouble(12, 3);
+            var testComplexNumberSum = testComplexNumberA + testComplexNumberB;
+            Assert.AreEqual("Вещественная часть 24, Мнимая 125", testComplexNumberSum.ComplexNumberDoubleToString());
+        }
+
+        [TestMethod()]
+        public void ComplexNumberDoubleAddingInt()
+        {
+            var testComplexNumberA = new ComplexNumberDouble(12, 122);
+            var testComplexNumberB = 100;
+            var testComplexNumberSum = testComplexNumberA + testComplexNumberB;
+            Assert.AreEqual("Вещественная часть 112, Мнимая 122", testComplexNumberSum.ComplexNumberDoubleToString());
+        }
+
+        [TestMethod()]
+        public void IntAddingComplexNumberDouble()
+        {
+            var testComplexNumberA = 304;
+            var testComplexNumberB = new ComplexNumberDouble(12, 122);
+            var testComplexNumberSum = testComplexNumberA + testComplexNumberB;
+            Assert.AreEqual("Вещественная часть 316, Мнимая 122", testComplexNumberSum.ComplexNumberDoubleToString());
+        }
+
+        [TestMethod()]
+        public void ComplexNumberDoubleSubtractingComplex()
+        {
+            var testComplexNumberA = new ComplexNumberDouble(12, 122);
+            var testComplexNumberB = new ComplexNumberDouble(12, 3);
+            var testComplexNumberSum = testComplexNumberA - testComplexNumberB;
+            Assert.AreEqual("Вещественная часть 0, Мнимая 119", testComplexNumberSum.ComplexNumberDoubleToString());
+        }
+
     }
 }
